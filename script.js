e85cdd9ff9f96ec5eb71de603ec6a40dcda22e9e
project <132855195+LcErhel@users.noreply.github.com>
2025-03-16 16:46:43 +0300

    add createGrid()

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+function createGrid() {
+    let amount = prompt("amount of squares? ", 16);
+    let container = document.querySelector(".container");
+    // let square = document.createElement("div").classList.add("square");
+    // container.appendChild(square);
+
+    outer:
+    for(let i = 0; i < amount; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.classList.add(`${i}`);
+        container.appendChild(row);
+
+        inner:
+        for(let y = 0; y < amount; y++) {
+            let square = document.createElement("div")
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+    }
+
+}
